b13098532f8a9ce78d6467c6880f76d337e690b6
Shubham <shubham.m.1306@gmail.com>
2025-03-31 13:07:50 +0530

    Add files via upload

---
+++ Question Bank (Oppe)/sep_o1_set-1/1-1.py
@@ -0,0 +1,12 @@
+def sum_squares_abs_diff_squares(a:int, b:int) -> tuple:
+    '''Return the sum of squares and the absolute difference of squares.
+
+    Args:
+        a, b : int - input numbers
+
+    Returns:
+        tuple - tuple with sum of squares and absolute difference of squares.
+
+    '''
+    ...
+    
